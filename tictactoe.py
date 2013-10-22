9f3735feb20a94fe3bf07032715dfa0564f4eb26
oliverswitzer <oliverswitzer@gmail.com>
2013-10-22 14:52:55 -0400

    created the main function and edited the showBoard function so it didnt give a str error

--- tictactoe.py
+++ tictactoe.py
@@ -3,14 +3,18 @@
 
 class Board:
 
-board = [0, 1, 2,
-		 3, 4, 5,
-		 6, 7, 8]
+	board = [0, 1, 2,
+			 3, 4, 5,
+			 6, 7, 8]
 
-	def showBoard():
-		print board[0] + "|" + board[1] + "|" + board[2]
+	def showBoard(self):
+		print self.board[0],"|",self.board[1],"|",self.board[2]
 		print "-" * 9
-		print board[3] + "|" + board[4] + "|" + board[5]
+		print self.board[3],"|",self.board[4],"|",self.board[5]
 		print "-" * 9
-		print board[6] + "|" + board[7] + "|" + board[8]
+		print self.board[6],"|",self.board[7],"|",self.board[8]
 		
+if __name__ == "__main__":
+	NewBoard = Board()
+
+	NewBoard.showBoard()
