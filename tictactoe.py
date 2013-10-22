817670bd23e3cb23f4c94906383adf8cb04d9b8e
oliverswitzer <oliverswitzer@gmail.com>
2013-10-22 15:02:44 -0400

    removed showboard funct and added __init__ for Board class

--- tictactoe.py
+++ tictactoe.py
@@ -7,14 +7,20 @@ class Board:
 			 3, 4, 5,
 			 6, 7, 8]
 
-	def showBoard(self):
+	def __init__(self):
+		print
+		print "Welcome! Care for a game of Tic Tac Toe?"
+		print 
 		print self.board[0],"|",self.board[1],"|",self.board[2]
 		print "-" * 9
 		print self.board[3],"|",self.board[4],"|",self.board[5]
 		print "-" * 9
 		print self.board[6],"|",self.board[7],"|",self.board[8]
+		print
 		
 if __name__ == "__main__":
 	NewBoard = Board()
 
-	NewBoard.showBoard()
+	running = True
+	# while running: 
+	# 	inp = int(raw_input("Choose a number on the board to place your x: ")
